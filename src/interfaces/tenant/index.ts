cb4bc8b2fdbdd731421872b205be81959a766b0b
Fernando Cortese <cortesefernando@gmail.com>
2022-04-04 17:04:49 -0300

    index

--- src/interfaces/tenant/index.ts
+++ src/interfaces/tenant/index.ts
@@ -1,4 +1,6 @@
 export * from "./cliente.dto";
 export * from "./cliente.model";
+export * from "./dispositivo.dto";
+export * from "./dispositivo.model";
 export * from "./usuario.dto";
 export * from "./usuario.model";
