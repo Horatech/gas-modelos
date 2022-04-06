d53bc356c5f3750280ce23f2eca526b0a99f9150
Fernando Cortese <cortesefernando@gmail.com>
2022-04-06 10:51:44 -0300

    tipo y appkey en dispositivo

--- src/interfaces/tenant/dispositivo.dto.ts
+++ src/interfaces/tenant/dispositivo.dto.ts
@@ -1,11 +1,17 @@
+export type TipoDispositivo = "NUC 4G" | "SML";
+
 export interface ICreateDispositivo {
   idCliente: string;
   deveui: string;
+  tipoDispotivivo: TipoDispositivo;
   deviceName?: string;
+  appkey?: string;
 }
 
 export interface IUpdateDispositivo {
   idCliente?: string;
   deveui?: string;
+  tipoDispotivivo?: TipoDispositivo;
   deviceName?: string;
+  appkey?: string;
 }
--- src/interfaces/tenant/dispositivo.model.ts
+++ src/interfaces/tenant/dispositivo.model.ts
@@ -1,7 +1,11 @@
+import { TipoDispositivo } from "./dispositivo.dto";
+
 export interface IDispositivo {
   _id: string;
   idCliente: string;
   deveui: string;
+  tipoDispotivivo: TipoDispositivo;
   deviceName?: string;
+  appkey?: string;
   fechaCreacion: string;
 }
