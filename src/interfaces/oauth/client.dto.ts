d87227972e9d3c2b964a69cf9c3a09149bbec02a
Fernando Cortese <cortesefernando@gmail.com>
2022-04-06 12:52:24 -0300

    id en client

--- src/interfaces/oauth/client.dto.ts
+++ src/interfaces/oauth/client.dto.ts
@@ -1,4 +1,5 @@
 export interface ICreateClient {
+  id: string;
   clientId: string;
   clientSecret: string;
   grants: string[];
--- src/interfaces/oauth/client.model.ts
+++ src/interfaces/oauth/client.model.ts
@@ -1,4 +1,5 @@
 export interface IClient {
+  id: string;
   clientId: string;
   clientSecret: string;
   grants: string[];
