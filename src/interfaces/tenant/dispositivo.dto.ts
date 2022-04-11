dbbe71c5e2f275b0c4587434428a9ed82b058d07
Fernando Cortese <cortesefernando@gmail.com>
2022-04-11 19:54:12 -0300

    tipo conectividad

--- src/interfaces/tenant/dispositivo.dto.ts
+++ src/interfaces/tenant/dispositivo.dto.ts
@@ -1,17 +1,20 @@
 export type TipoDispositivo = "NUC 4G" | "SML";
+export type TipoConectividad = "4G" | "LORA";
 
 export interface ICreateDispositivo {
   idCliente: string;
   deveui: string;
-  tipoDispositivo: TipoDispositivo;
   deviceName?: string;
   appkey?: string;
+  tipoDispositivo: TipoDispositivo;
+  conectividad: TipoConectividad;
 }
 
 export interface IUpdateDispositivo {
   idCliente?: string;
   deveui?: string;
-  tipoDispositivo?: TipoDispositivo;
   deviceName?: string;
   appkey?: string;
+  tipoDispositivo?: TipoDispositivo;
+  conectividad: TipoConectividad;
 }
--- src/interfaces/tenant/dispositivo.model.ts
+++ src/interfaces/tenant/dispositivo.model.ts
@@ -1,11 +1,12 @@
-import { TipoDispositivo } from "./dispositivo.dto";
+import { TipoConectividad, TipoDispositivo } from "./dispositivo.dto";
 
 export interface IDispositivo {
   _id: string;
+  fechaCreacion: string;
   idCliente: string;
   deveui: string;
-  tipoDispositivo: TipoDispositivo;
   deviceName?: string;
   appkey?: string;
-  fechaCreacion: string;
+  tipoDispositivo: TipoDispositivo;
+  conectividad: TipoConectividad;
 }
