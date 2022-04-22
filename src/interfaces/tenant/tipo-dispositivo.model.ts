48f2805977d75cccf9691bd4f53bcac005e49c94
Fernando Cortese <cortesefernando@gmail.com>
2022-04-22 08:47:44 -0300

    tipo disp

--- src/interfaces/tenant/tipo-dispositivo.model.ts
+++ src/interfaces/tenant/tipo-dispositivo.model.ts
@@ -1,6 +1,7 @@
 export type TipoDispositivo = "NUC" | "SML";
 
 export interface ITipoDispositivo {
+  _id: string;
   nombre: TipoDispositivo;
   deviceProfileID?: string;
   integrationUrl?: string;
