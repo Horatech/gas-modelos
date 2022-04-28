e9f58825464fcdb0f5287b782b735255c4d90445
Fernando Cortese <cortesefernando@gmail.com>
2022-04-28 15:43:05 -0300

    fix

--- src/interfaces/gas/resumen.dto.ts
+++ src/interfaces/gas/resumen.dto.ts
@@ -1,9 +1,10 @@
-import { ICorrectora } from "./correctora.model";
+import { IUnidadNegocio } from "./unidadNegocio.model";
 import { ICuenca } from "./cuenca.model";
+import { ICorrectora } from "./correctora.model";
 
 // Resumen
 export interface IResumenUnidadNegocio {
-  unidadNegocio: IResumenUnidadNegocio;
+  unidadNegocio: IUnidadNegocio;
   cuencas: {
     cuenca: ICuenca;
     correctoras: ICorrectora[];
