dce055742768368e4f0fc7adcefa8079da745332
Fernando Cortese <cortesefernando@gmail.com>
2022-09-14 10:44:18 -0300

    update resumen por unidad de negocio

--- src/interfaces/gas/resumen.dto.ts
+++ src/interfaces/gas/resumen.dto.ts
@@ -3,18 +3,37 @@ import { ICuenca } from "./cuenca.model";
 import { ICorrectora } from "./correctora.model";
 import { ICliente } from "../tenant";
 import { IMedidorResidencial } from "./medidorResidencial.model";
+import { ICentroOperativo } from "./centroOperativo.model";
 
 // Resumen
 export interface IResumenUnidadNegocio {
   unidadNegocio: IUnidadNegocio;
+  centroOperativos: {
+    cantReportesOkCorrectora: number;
+    cantReportesOkMedidor: number;
+    porcentajeReporteCorrectoras: number;
+    porcentajeReporteMedidores: number;
+    centroOperativo: ICentroOperativo;
+    correctoras: ICorrectora[];
+    medidores: IMedidorResidencial[];
+  };
   cuencas: {
+    cantReportesOkCorrectora: number;
+    cantReportesOkMedidor: number;
+    porcentajeReporteCorrectoras: number;
+    porcentajeReporteMedidores: number;
     cuenca: ICuenca;
     correctoras: ICorrectora[];
     medidores: IMedidorResidencial[];
   }[];
   cantidadCuencas: number;
+  cantidadCentroOperativos: number;
   cantidadCorrectoras: number;
   cantidadMedidores: number;
+  cantReportesOkCorrectora: number;
+  cantReportesOkMedidor: number;
+  porcentajeReporteCorrectoras: number;
+  porcentajeReporteMedidores: number;
 }
 
 export interface IResumenCliente {
