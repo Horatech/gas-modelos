e76be19fae6420edd346e68d53a3f54065368205
Tatoh <tatukis@gmail.com>
2022-12-19 12:08:25 -0300

    ni idea

---
+++ src/interfaces/gas/medidorResidencialAgua.dto.ts
@@ -0,0 +1,45 @@
+import { ICoordenadas } from '../auxiliares';
+import { ITipoDispositivo } from '../tenant';
+import { IReporteSml } from './reporteSml.model';
+
+export interface ICreateMedidorResidencialAgua {
+  deviceMeterNumber: number;
+  deveui: string;
+  deviceName?: string;
+  //
+  ultimoReporte?: IReporteSml;
+  //
+  consumoInicial?: number;
+  ubicacionGps?: ICoordenadas;
+  direccion?: string;
+  localidad?: string;
+  nombre?: string;
+  descripcion?: string;
+  //
+  idCliente?: string;
+  idUnidadNegocio?: string;
+  idCentroOperativo?: string;
+  idCuenca?: string;
+  idsGrupos?: string[];
+}
+
+export interface IUpdateMedidorResidencialAgua {
+  deviceMeterNumber?: number;
+  deveui?: string;
+  deviceName?: string;
+  //
+  ultimoReporte?: IReporteSml;
+  //
+  consumoInicial?: number;
+  ubicacionGps?: ICoordenadas;
+  direccion?: string;
+  localidad?: string;
+  nombre?: string;
+  descripcion?: string;
+  //
+  idCliente?: string;
+  idUnidadNegocio?: string;
+  idCentroOperativo?: string;
+  idCuenca?: string;
+  idsGrupos?: string[];
+}
