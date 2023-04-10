439e5ad74a7c584a62413406c0f99fbc67fe8d44
Fernando Cortese <cortesefernando@gmail.com>
2023-04-10 16:45:04 -0300

    update

--- src/interfaces/gas/reporteSml.dto.ts
+++ src/interfaces/gas/reporteSml.dto.ts
@@ -1,16 +1,16 @@
 export interface ICreateReporteSml {
-  header: number;
-  paquetSequence: number;
-  deviceMeterNumber: number;
-  pulseConstant: number;
-  meterType: number;
-  meteringMode: number;
-  consumo: number;
-  consumoNegativo: number | null;
-  batteryVoltage: number;
-  statusWord: number;
-  triggerSource: number;
-  checksum: number;
+  header?: number;
+  paquetSequence?: number;
+  deviceMeterNumber?: number;
+  pulseConstant?: number;
+  meterType?: number;
+  meteringMode?: number;
+  consumo?: number;
+  consumoNegativo?: number | null;
+  batteryVoltage?: number;
+  statusWord?: number;
+  triggerSource?: number;
+  checksum?: number;
   //
   fecha: string;
   idCliente: string;
--- src/interfaces/gas/reporteSml.model.ts
+++ src/interfaces/gas/reporteSml.model.ts
@@ -1,17 +1,17 @@
 export interface IReporteSml {
   _id: string;
-  header: number;
-  paquetSequence: number;
-  deviceMeterNumber: number;
-  pulseConstant: number;
-  meterType: number;
-  meteringMode: number;
-  consumo: number;
-  consumoNegativo: number | null;
-  batteryVoltage: number;
-  statusWord: number;
-  triggerSource: number;
-  checksum: number;
+  header?: number;
+  paquetSequence?: number;
+  deviceMeterNumber?: number;
+  pulseConstant?: number;
+  meterType?: number;
+  meteringMode?: number;
+  consumo?: number;
+  consumoNegativo?: number | null;
+  batteryVoltage?: number;
+  statusWord?: number;
+  triggerSource?: number;
+  checksum?: number;
   //
   fecha: string;
   idCliente: string;
