01ceb544b6b38f14e17d0787ce1afb4a93331f5b
Ignacio Martín Arano <tatukis@gmail.com>
2023-04-26 11:22:15 -0300

    tipo

--- src/interfaces/gas/logNuc/create.ts
+++ src/interfaces/gas/logNuc/create.ts
@@ -10,10 +10,12 @@ import {
   ISetRegistroV3,
   ISetReporte,
   ISetReporteV3,
+  TipoMensaje,
 } from './extras';
 
 export interface ICreateLogNuc {
   deveui: string;
+  tipo: TipoMensaje;
   body:
     | ISetAlerta
     | ISetAlertaV2
