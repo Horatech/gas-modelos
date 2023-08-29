cf8a4e19d836dc36819d0d83f5eb66b550726117
Fernando Cortese <cortesefernando@gmail.com>
2023-08-29 14:47:02 -0300

    quitados campos de correctoras que pasaron al punto de medicion

--- src/interfaces/gas/correctora/schema.ts
+++ src/interfaces/gas/correctora/schema.ts
@@ -1,12 +1,9 @@
-import { ICoordenadas } from '../../auxiliares';
-import { IDispositivo } from '../../tenant';
-import { IAlerta } from '../alerta';
-import { ICentroOperativo } from '../centroOperativo';
-import { ICromatografia } from '../cromatografia';
-import { ICuenca } from '../cuenca';
-import { IGrupo } from '../grupo';
-import { IRegistro } from '../registro';
-import { IUnidadNegocio } from '../unidadNegocio';
+import { IDispositivo } from "../../tenant";
+import { IAlerta } from "../alerta";
+import { ICentroOperativo } from "../centroOperativo";
+import { ICromatografia } from "../cromatografia";
+import { IRegistro } from "../registro";
+import { IUnidadNegocio } from "../unidadNegocio";
 
 export interface ICorrectora {
   _id: string;
@@ -14,7 +11,6 @@ export interface ICorrectora {
   firmware: string;
   numeroSerie?: string | null;
   deveui?: string | null;
-  deviceName?: string;
   modelo: string;
   fechaCreacion: string;
   bateria?: number;
@@ -24,23 +20,13 @@ export interface ICorrectora {
   ultimaCromatografia?: ICromatografia;
   fechaUltimaCromatografia?: string;
   //
-  ubicacionGps?: ICoordenadas;
-  direccion?: string;
-  localidad?: string;
-  nombre?: string;
-  descripcion?: string;
-  codigoSimec?: string;
-  estadoActual?: 'Operativa' | 'En Mantenimiento' | 'Resolver' | string;
+  estadoActual?: "Operativa" | "En Mantenimiento" | "Resolver" | string;
   //
   idCliente: string;
   idUnidadNegocio?: string;
   idCentroOperativo?: string;
-  idCuenca?: string;
-  idsGrupos?: string[];
   // Populate
   unidadNegocio?: IUnidadNegocio;
   centroOperativo?: ICentroOperativo;
-  cuenca?: ICuenca;
-  grupos?: IGrupo[];
   dispositivo?: IDispositivo;
 }
--- src/interfaces/gas/correctora/update.ts
+++ src/interfaces/gas/correctora/update.ts
@@ -1,13 +1,12 @@
-import { ICoordenadas } from '../../auxiliares';
-import { IAlerta } from '../alerta';
-import { ICromatografia } from '../cromatografia';
-import { IRegistro } from '../registro';
+import { ICoordenadas } from "../../auxiliares";
+import { IAlerta } from "../alerta";
+import { ICromatografia } from "../cromatografia";
+import { IRegistro } from "../registro";
 
 export interface IUpdateCorrectora {
   firmware?: string;
   numeroSerie?: string | null;
   deveui?: string | null;
-  deviceName?: string;
   modelo?: string;
   bateria?: number;
   //
@@ -16,17 +15,9 @@ export interface IUpdateCorrectora {
   ultimaCromatografia?: ICromatografia;
   fechaUltimaCromatografia?: string;
   //
-  ubicacionGps?: ICoordenadas;
-  direccion?: string;
-  localidad?: string;
-  nombre?: string;
-  descripcion?: string;
-  codigoSimec?: string;
-  estadoActual?: 'Operativa' | 'En Mantenimiento' | 'Resolver' | string;
+  estadoActual?: "Operativa" | "En Mantenimiento" | "Resolver" | string;
   //
   idCliente?: string;
   idUnidadNegocio?: string;
   idCentroOperativo?: string;
-  idCuenca?: string;
-  idsGrupos?: string[];
 }
