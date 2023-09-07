748e66000680ffbf4a22840ef373b8fb8093400b
Fernando Cortese <cortesefernando@gmail.com>
2023-09-07 10:33:30 -0300

    update modelo envio smss

--- src/interfaces/gas/envioSms/schema.ts
+++ src/interfaces/gas/envioSms/schema.ts
@@ -1,24 +1,27 @@
+import { IUsuario } from "../../tenant";
 import { ICentroOperativo } from "../centroOperativo";
 import { IGrupo } from "../grupo";
 import { IUnidadNegocio } from "../unidadNegocio";
 
 export type TipoAlerta = "Unidades Presión - Fuera de límite ";
 
+export type Agrupacion = "Unidad de Negocio" | "Centro Operativo" | "Grupo";
+
 export interface IEnvioSms {
   _id?: string;
   idCliente?: string;
   tiposAlerta?: TipoAlerta[];
+  agrupacion?: Agrupacion;
 
   idUnidadNegocio?: string | null;
   idCentroOperativo?: string | null;
   idsGrupos?: string[] | null;
 
-  telefonosUnidadNegocio?: string[] | null;
-  telefonosCentroOperativo?: string[] | null;
-  telefonosGrupos?: string[] | null;
+  idsUsuarios?: string[];
 
   // Virtual
   unidadNegocio?: IUnidadNegocio;
   centroOperativo?: ICentroOperativo;
   grupos?: IGrupo[];
+  usuarios?: IUsuario[];
 }
--- src/interfaces/gas/envioSms/update.ts
+++ src/interfaces/gas/envioSms/update.ts
@@ -1,14 +1,13 @@
-import { TipoAlerta } from "./schema";
+import { Agrupacion, TipoAlerta } from "./schema";
 
 export interface IUpdateEnvioSms {
   idCliente?: string;
   tiposAlerta?: TipoAlerta[];
+  agrupacion?: Agrupacion;
 
   idUnidadNegocio?: string | null;
   idCentroOperativo?: string | null;
   idsGrupos?: string[] | null;
 
-  telefonosUnidadNegocio?: string[] | null;
-  telefonosCentroOperativo?: string[] | null;
-  telefonosGrupos?: string[] | null;
+  idsUsuarios?: string[];
 }
