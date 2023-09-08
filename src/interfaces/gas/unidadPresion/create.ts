acb6c9812d8b7f25e13fa7f7bb987c847eed7091
Fernando Cortese <cortesefernando@gmail.com>
2023-09-08 09:46:05 -0300

    update unidad presion

--- src/interfaces/gas/unidadPresion/create.ts
+++ src/interfaces/gas/unidadPresion/create.ts
@@ -1,29 +1,17 @@
-import { GeoJSON, ICoordenadas } from '../../auxiliares';
-import { IAlerta } from '../alerta';
-import { IReporte } from '../reporte';
+import { IAlerta } from "../alerta";
+import { IReporte } from "../reporte";
 
 export interface ICreateUnidadPresion {
-  firmware?: string;
+  modelo?: string;
   numeroSerie?: string | null;
   deveui?: string | null;
-  deviceName?: string;
-  modelo?: string;
-  bateria?: number;
   //
   ultimoRegistro?: IReporte;
   ultimaAlerta?: IAlerta;
   //
-  ubicacion?: ICoordenadas;
-  geojson?: GeoJSON;
-  direccion?: string;
-  localidad?: string;
-  nombre?: string;
-  descripcion?: string;
-  estadoActual?: 'Operativa' | 'En Mantenimiento' | 'Resolver' | string;
+  estadoActual?: "Operativa" | "En Mantenimiento" | "Resolver" | string;
   //
   idCliente: string;
   idUnidadNegocio?: string;
   idCentroOperativo?: string;
-  idCuenca?: string;
-  idsGrupos?: string[];
 }
