41ef887d95de47af79687d216b7f499bc86f1441
Ignacio Martín Arano <tatukis@gmail.com>
2023-09-22 12:13:52 -0300

    IFIlter chitero

--- src/interfaces/auxiliares/queryParams.ts
+++ src/interfaces/auxiliares/queryParams.ts
@@ -7,14 +7,34 @@ export interface IQueryParam {
   select?: string;
   [key: string]: any;
 }
-
-export interface IFilter {
-  [key: string]: any;
-}
-
 export interface IPopulate {
   path?: string;
   select?: string;
   populate?: IPopulate;
   [key: string]: any;
 }
+
+type mongoOperators = {
+  $regex?: string;
+  $in?: string[];
+  $nin?: string[];
+  $gte?: number | string;
+  $lte?: number | string;
+  $gt?: number | string;
+  $lt?: number | string;
+  $eq?: number | string | boolean | null;
+  $ne?: number | string | boolean | null;
+  $exists?: boolean;
+  [key: string]: any;
+};
+
+type mongoFilter = number | string | boolean | null | mongoOperators;
+
+export type IFilter<T> = {
+  [K in keyof T]: undefined | mongoFilter | IFilter<T>[];
+} & {
+  $or?: IFilter<T>[];
+  $and?: IFilter<T>[];
+  $nor?: IFilter<T>[];
+  $not?: mongoFilter;
+};
