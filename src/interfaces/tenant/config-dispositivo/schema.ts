7965b878977d69b1997d9e6b8e107297684ea620
Ignacio Martín Arano <tatukis@gmail.com>
2024-01-22 14:04:40 -0300

    Update schema.ts with consistent quotation marks

--- src/interfaces/tenant/config-dispositivo/schema.ts
+++ src/interfaces/tenant/config-dispositivo/schema.ts
@@ -1,11 +1,11 @@
-import { IDispositivo } from "../dispositivo.model";
+import { IDispositivo } from '../dispositivo.model';
 
 export interface IConfigDispositivoNUC4G {
   horaInicio: number;
-  modoOperacion: "REG1_DIARIO" | "REG24_DIARIO" | "REG8_8HORAS";
-  modoEnv: "TEST" | "PROD";
+  modoOperacion: 'REG1_DIARIO' | 'REG24_DIARIO' | 'REG8_8HORAS';
+  modoEnv: 'TEST' | 'PROD';
   claveMercury: string;
-  modoRegistros: "REG_TOTALIZADOS" | "REG_PARCIALES";
+  modoRegistros: 'REG_TOTALIZADOS' | 'REG_PARCIALES';
 }
 
 export interface IConfigDispositivoNSP4G {
@@ -13,18 +13,28 @@ export interface IConfigDispositivoNSP4G {
   limiteMax?: number;
   horaUTC?: number;
   horaInicio?: number;
-  modoEnv?: "TEST" | "PROD";
+  modoEnv?: 'TEST' | 'PROD';
   modoOperacion?:
-    | "REG1_DIARIO"
-    | "REG24_DIARIO"
-    | "REG1_1HORA"
-    | "REG2_2HORAS"
-    | "REG3_3HORAS"
-    | "REG4_4HORAS"
-    | "REG6_6HORAS"
-    | "REG8_8HORAS"
-    | "REG12_12HORAS";
+    | 'REG1_DIARIO'
+    | 'REG24_DIARIO'
+    | 'REG1_1HORA'
+    | 'REG2_2HORAS'
+    | 'REG3_3HORAS'
+    | 'REG4_4HORAS'
+    | 'REG6_6HORAS'
+    | 'REG8_8HORAS'
+    | 'REG12_12HORAS';
   timestampBloqueo?: number;
+}
+
+export interface IConfigDispositivoVeribox {
+  telefono?: string;
+  frecuenciaComunicacion?: number;
+  limiteMin?: number;
+  limiteMax?: number;
+  apn?: string;
+  usuario?: string;
+  clave?: string;
 }
 
 export interface IConfigDispositivo {
