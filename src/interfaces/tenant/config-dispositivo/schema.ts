2cd0ce7ed420cb41f2a99c17f7b8596a46ed1420
Fernando Cortese <cortesefernando@gmail.com>
2024-02-01 11:57:57 -0300

    Update schema.ts for config-dispositivo interface

--- src/interfaces/tenant/config-dispositivo/schema.ts
+++ src/interfaces/tenant/config-dispositivo/schema.ts
@@ -1,11 +1,12 @@
-import { IDispositivo } from '../dispositivo.model';
+import { IDispositivo } from "../dispositivo.model";
 
 export interface IConfigDispositivoNUC4G {
   horaInicio: number;
-  modoOperacion: 'REG1_DIARIO' | 'REG24_DIARIO' | 'REG8_8HORAS';
-  modoEnv: 'TEST' | 'PROD';
+  modoOperacion: "REG1_DIARIO" | "REG24_DIARIO" | "REG8_8HORAS";
+  modoEnv: "TEST" | "PROD";
   claveMercury: string;
-  modoRegistros: 'REG_TOTALIZADOS' | 'REG_PARCIALES';
+  modoRegistros: "REG_TOTALIZADOS" | "REG_PARCIALES";
+  nsa?: number; // Numero de serie de american meter
 }
 
 export interface IConfigDispositivoNSP4G {
@@ -13,17 +14,17 @@ export interface IConfigDispositivoNSP4G {
   limiteMax?: number;
   horaUTC?: number;
   horaInicio?: number;
-  modoEnv?: 'TEST' | 'PROD';
+  modoEnv?: "TEST" | "PROD";
   modoOperacion?:
-    | 'REG1_DIARIO'
-    | 'REG24_DIARIO'
-    | 'REG1_1HORA'
-    | 'REG2_2HORAS'
-    | 'REG3_3HORAS'
-    | 'REG4_4HORAS'
-    | 'REG6_6HORAS'
-    | 'REG8_8HORAS'
-    | 'REG12_12HORAS';
+    | "REG1_DIARIO"
+    | "REG24_DIARIO"
+    | "REG1_1HORA"
+    | "REG2_2HORAS"
+    | "REG3_3HORAS"
+    | "REG4_4HORAS"
+    | "REG6_6HORAS"
+    | "REG8_8HORAS"
+    | "REG12_12HORAS";
   timestampBloqueo?: number;
 }
 
