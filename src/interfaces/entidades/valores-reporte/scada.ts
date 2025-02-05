807c91e459f7529e7760a3f17e8605d2b413d19c
Tatoh <tatukis@gmail.com>
2025-02-05 10:34:19 -0300

    Update IReporteScada interface to allow null values for limit properties for better data handling

--- src/interfaces/entidades/valores-reporte/scada.ts
+++ src/interfaces/entidades/valores-reporte/scada.ts
@@ -1,11 +1,17 @@
 import { TipoScada } from '../scada';
 
+/**
+ * @property limiteHH - Limite superior alto es null cuando no existe
+ * @property limiteH - Limite superior es null cuando no existe
+ * @property limiteLL - Limite inferior bajo es null cuando no existe
+ * @property limiteL - Limite inferior es null cuando no existe
+ */
 export interface IReporteScada {
   tipo?: TipoScada;
   timestamp?: string;
   valorActual?: number;
-  limiteHH?: number;
-  limiteH?: number;
-  limiteLL?: number;
-  limiteL?: number;
+  limiteHH?: number | null;
+  limiteH?: number | null;
+  limiteLL?: number | null;
+  limiteL?: number | null;
 }
