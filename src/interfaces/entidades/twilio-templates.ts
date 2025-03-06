8e3b1c19b96e0a605031d948186c75641a8633cb
Tatoh <tatukis@gmail.com>
2025-03-06 10:25:16 -0300

    Refactor Twilio message templates to enhance structure and add missing properties

--- src/interfaces/entidades/twilio-templates.ts
+++ src/interfaces/entidades/twilio-templates.ts
@@ -1,81 +1,149 @@
 /**
- * Interfaz que define la estructura de los templates de mensajes de Twilio
- * para enviar mensajes de alarma de presión por WhatsApp
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
  * @example 'Estado de presión {{1}} en el punto de medición {{2}} con un valor de {{3}}. Horatech.'
  * @param estado Estado de la presión {{1}}
  * @param punto Punto de medición {{2}}
  * @param valor Valor de la presión {{3}}
+ * @param sid HXfb4194701ce4baf115447c6b0baad06c
  */
-export interface ITwilioLimitePresionWA {
+export interface ITwilioPresionTemplate {
   estado: string;
   punto: string;
   valor: string;
+  sid: string;
 }
 
 /**
- * Interfaz que define la estructura de los templates de mensajes de Twilio
- * para enviar mensajes de alarma de presión por WhatsApp
- * @example 'Estado de presión {{1}} en el punto de medición {{2}} con un valor de {{3}} con fecha {{4}}. Horatech.'
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'Punto de medición {{1}} pasado a estado En Mantenimiento. Horatech.'
+ * @param punto Punto de medición {{1}}
+ * @param sid HX36663f2a4ef4b29455df5cc6c6eb00a8
+ */
+export interface ITwilioMantenimientoTemplate {
+  punto: string;
+  sid: string;
+}
+
+/**
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'Sensor deconectado en el punto de medición {{1}}. Horatech.'
+ * @param punto Punto de medición {{1}}
+ * @param sid HXa73924c271c567499fc61c8e3c629909
+ */
+export interface ITwilioSensorDesconectadoTemplate {
+  punto: string;
+  sid: string;
+}
+
+/**
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'Error de comunicación de alarma. Estado de presión {{1}} en el punto de medición {{2}} con un valor de {{3}} con fecha {{4}}. Horatech.'
  * @param estado Estado de la presión {{1}}
  * @param punto Punto de medición {{2}}
  * @param valor Valor de la presión {{3}}
- * @param fecha Fecha de la alarma {{4}}
+ * @param fecha Fecha del error {{4}}
+ * @param sid HX30c8f98ac067a102c2ec970482b393ba
  */
-export interface ITwilioErrorComunicacionAlarmaWA {
+export interface ITwilioErrorComunicacionAlarmaTemplate {
   estado: string;
   punto: string;
   valor: string;
   fecha: string;
+  sid: string;
 }
 
 /**
- * Interfaz que define la estructura de los templates de mensajes de Twilio
- * para enviar mensajes de alarma de presión por SMS
- * @example 'Estado de presión {{1}} en el punto de medición {{2}} con un valor de {{3}}. Horatech.'
- * @param estado Estado de la presión {{1}}
- * @param punto Punto de medición {{2}}
- * @param valor Valor de la presión {{3}}
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'Se ha restablecido el valor en el punto {{1}}. Horatech.'
+ * @param punto Punto de medición {{1}}
+ * @param sid HX79264e34254ed1f65121644a3f4d8979
  */
-export interface ITwilioLimitePresionSMS {
-  estado: string;
+export interface ITwilioScadaBooleanoReestablecidoTemplate {
+  punto: string;
+  sid: string;
+}
+
+/**
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'El valor {{1}} ha vuelto a estar dentro los niveles normales en el punto {{2}}. Horatech.'
+ * @param valor Valor {{1}}
+ * @param punto Punto de medición {{2}}
+ * @param sid HX9bb2b5180e4d9a25b67423bc2e5eb43f
+ */
+export interface ITwilioScadaValorReestablecidoTemplate {
+  valor: string;
+  punto: string;
+  sid: string;
+}
+
+/**
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'El límite {{1}} del punto {{2}} ha sido cambiado a {{3}}. Horatech.'
+ * @param limite Límite {{1}}
+ * @param punto Punto de medición {{2}}
+ * @param valor Valor {{3}}
+ * @param sid HX062696564a7a45eb649751fb6bb98220
+ */
+export interface ITwilioScadaCambioLimiteTemplate {
+  limite: string;
   punto: string;
   valor: string;
+  sid: string;
 }
 
 /**
- * Interfaz que define la estructura de los templates de mensajes de Twilio
- * para enviar mensajes de alarma de presión por SMS
- * @example 'Estado de presión {{1}} en el punto de medición {{2}} con un valor de {{3}} con fecha {{4}}. Horatech.'
- * @param estado Estado de la presión {{1}}
- * @param punto Punto de medición {{2}}
- * @param valor Valor de la presión {{3}}
- * @param fecha Fecha de la alarma {{4}}
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'El punto {{1}} ha reportado el valor {{2}}. El valor de alarma es {{3}}. Horatech.'
+ * @param punto Punto de medición {{1}}
+ * @param valor Valor {{2}}
+ * @param alarma Valor de alarma {{3}}
+ * @param sid HXe87e6d98251d55a6a6ddf000c3146b75
  */
-export interface ITwilioErrorComunicacionAlarmaSMS {
-  estado: string;
+export interface ITwilioScadaBooleanoAlertaTemplate {
   punto: string;
   valor: string;
-  fecha: string;
+  alarma: string;
+  sid: string;
 }
 
 /**
- * Interfaz que define la estructura de los templates de mensajes de Twilio
- * para enviar mensajes de alarma de presión por SMS
- * @example 'Punto de medición {{1}} pasado a estado En Mantenimiento. Horatech.'
- * @param punto Punto de medición {{1}}
+ * Los templates de mensajes de Twilio son mensajes predefinidos que se pueden
+ * enviar a través de la API de Twilio. Estos mensajes pueden ser personalizados
+ * con variables que se reemplazan por valores específicos al momento de enviar
+ * el mensaje.
+ * @example 'El valor {{1}} ha sobrepasado el nivel {{2}} en el punto {{3}}. Horatech.'
+ * @param valor Valor {{1}}
+ * @param nivel Nivel {{2}}
+ * @param punto Punto de medición {{3}}
+ * @param sid HX77a6e9216b71d1ab0a0f7ecb479956c4
  */
-
-export interface ITwilioMantenimientoWA {
+export interface ITwilioScadaBooleanoAlertaTemplate {
+  valor: string;
+  nivel: string;
   punto: string;
-}
-
-/**
- * Interfaz que define la estructura de los templates de mensajes de Twilio
- * para enviar mensajes de alarma de presión por SMS
- * @example 'Punto de medición {{1}} pasado a estado En Mantenimiento. Horatech.'
- * @param punto Punto de medición {{1}}
- */
-
-export interface ITwilioMantenimientoSMS {
-  punto: string;
+  sid: string;
 }
