dfef904f1f16e23b456bb6c429f584c1da30c3a5
Fernando Cortese <cortesefernando@gmail.com>
2025-05-21 11:28:52 -0300

    Reorganizar la interfaz IDispositivoNuc4G para establecer configuraciones obligatorias y mejorar la claridad del código

--- src/interfaces/entidades/configs-dispositivo/dispositivoNuc4G.ts
+++ src/interfaces/entidades/configs-dispositivo/dispositivoNuc4G.ts
@@ -1,9 +1,21 @@
 export interface IDispositivoNuc4G {
-  firmwareNuc?: string;
-  apiVersion?: string;
+  // Lo que viene en set configuracion
+  deveui: string;
+  appkey: string;
+  firmwareNuc: string;
+  apiVersion: string;
+  horaInicio: number;
+  modoOperacion: "REG1_DIARIO" | "REG24_DIARIO" | "REG8_8HORAS";
+  modoEnv: "TEST" | "PROD";
+  claveMercury: string;
+  modoRegistros: "REG_TOTALIZADOS" | "REG_PARCIALES";
+  iccid: number;
+  operadora: string;
+  nsa: number;
+  voltajeBateria: number;
+  versionHardware: string;
+  //
   frecuenciaComunicacion?: number;
-  horaInicio?: number;
-  modoOperacion?: string;
   redPreferida?: string;
   desfaceHorario?: number; // Min
 }
