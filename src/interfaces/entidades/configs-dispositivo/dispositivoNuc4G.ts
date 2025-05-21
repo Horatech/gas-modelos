87cd69fb911c69ee9eca5a6bffc0885fc63e8135
Fernando Cortese <cortesefernando@gmail.com>
2025-05-21 11:31:11 -0300

    Hacer opcionales los campos de configuración en la interfaz IDispositivoNuc4G para mayor flexibilidad

--- src/interfaces/entidades/configs-dispositivo/dispositivoNuc4G.ts
+++ src/interfaces/entidades/configs-dispositivo/dispositivoNuc4G.ts
@@ -1,19 +1,19 @@
 export interface IDispositivoNuc4G {
   // Lo que viene en set configuracion
-  deveui: string;
-  appkey: string;
-  firmwareNuc: string;
-  apiVersion: string;
-  horaInicio: number;
-  modoOperacion: "REG1_DIARIO" | "REG24_DIARIO" | "REG8_8HORAS";
-  modoEnv: "TEST" | "PROD";
-  claveMercury: string;
-  modoRegistros: "REG_TOTALIZADOS" | "REG_PARCIALES";
-  iccid: number;
-  operadora: string;
-  nsa: number;
-  voltajeBateria: number;
-  versionHardware: string;
+  deveui?: string;
+  appkey?: string;
+  firmwareNuc?: string;
+  apiVersion?: string;
+  horaInicio?: number;
+  modoOperacion?: "REG1_DIARIO" | "REG24_DIARIO" | "REG8_8HORAS";
+  modoEnv?: "TEST" | "PROD";
+  claveMercury?: string;
+  modoRegistros?: "REG_TOTALIZADOS" | "REG_PARCIALES";
+  iccid?: number;
+  operadora?: string;
+  nsa?: number;
+  voltajeBateria?: number;
+  versionHardware?: string;
   //
   frecuenciaComunicacion?: number;
   redPreferida?: string;
