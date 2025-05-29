bf9dd0f8d6817aaa03dff4bca497bb766966bb77
Fernando Cortese <cortesefernando@gmail.com>
2025-05-29 16:48:06 -0300

    Ajustar el formato de importaciones y agregar comentario para 'idsAsignadosHash' en la interfaz IReporte

--- src/interfaces/entidades/reporte.ts
+++ src/interfaces/entidades/reporte.ts
@@ -1,11 +1,11 @@
-import { IDeviceInfo, ITenantInfo } from '../auxiliares';
-import { ICorrectora } from './correctora';
-import { IMedidorResidencial } from './medidor-residencial';
-import { IMedidorResidencialAgua } from './medidor-residencial-agua';
-import { IScada } from './scada';
-import { IPuntoMedicion } from './punto-medicion';
-import { IUnidadPresion } from './unidad-presion';
-import { IValoresReporte } from './valores-reporte/valoresReporte';
+import { IDeviceInfo, ITenantInfo } from "../auxiliares";
+import { ICorrectora } from "./correctora";
+import { IMedidorResidencial } from "./medidor-residencial";
+import { IMedidorResidencialAgua } from "./medidor-residencial-agua";
+import { IScada } from "./scada";
+import { IPuntoMedicion } from "./punto-medicion";
+import { IUnidadPresion } from "./unidad-presion";
+import { IValoresReporte } from "./valores-reporte/valoresReporte";
 
 export interface IReporte {
   _id?: string;
@@ -16,6 +16,7 @@ export interface IReporte {
   device?: IDeviceInfo;
   // Ids de otras entidades que tienen asignado el dispositivo
   idsAsignados?: string[];
+  idsAsignadosHash?: string; // Esto es un string para usar como indice unico los ids asignados
   // Datos especificos de acuerdo al tipo de dispositivo
   valores?: IValoresReporte;
 
@@ -30,13 +31,13 @@ export interface IReporte {
 
 ////// CREATE
 type Omitir =
-  | '_id'
-  | 'puntoMedicion'
-  | 'correctora'
-  | 'unidadPrsion'
-  | 'medidorResidencial'
-  | 'medidorResidencialAgua'
-  | 'scada';
+  | "_id"
+  | "puntoMedicion"
+  | "correctora"
+  | "unidadPrsion"
+  | "medidorResidencial"
+  | "medidorResidencialAgua"
+  | "scada";
 export interface ICreateReporte extends Omit<Partial<IReporte>, Omitir> {}
 
 export interface IUpdateReporte extends Omit<Partial<IReporte>, Omitir> {}
