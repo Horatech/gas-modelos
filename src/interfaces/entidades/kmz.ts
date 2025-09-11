8c901a4ab98e085d842014e4a269f973446b9f44
Fernando Cortese <cortesefernando@gmail.com>
2025-09-11 14:15:34 -0300

    Actualizar la interfaz IKmz: agregar campos idUnidadNegocio e idCentroOperativo, y ajustar las omisiones en ICreateKmz e IUpdateKmz.

--- src/interfaces/entidades/kmz.ts
+++ src/interfaces/entidades/kmz.ts
@@ -1,19 +1,29 @@
+import { ICentroOperativo, IUnidadNegocio } from "../gas";
 import { ICliente } from "../tenant";
 
 export interface IKmz {
   _id?: string;
   idCliente?: string;
+  idUnidadNegocio?: string;
+  idCentroOperativo?: string;
   nombre?: string;
   urlKmz?: string;
 
   // Virtuals
   cliente?: ICliente;
+  unidadNegocio?: IUnidadNegocio;
+  centroOperativo?: ICentroOperativo;
 }
 
 // CREATE
-type OmitirCreate = "_id" | "cliente";
+type OmitirCreate = "_id" | "cliente" | "unidadNegocio" | "centroOperativo";
 export interface ICreateKmz extends Omit<Partial<IKmz>, OmitirCreate> {}
 
 // UPDATE
-type OmitirUpdate = "_id" | "cliente";
+type OmitirUpdate =
+  | "_id"
+  | "cliente"
+  | "unidadNegocio"
+  | "centroOperativo"
+  | "idCliente";
 export interface IUpdateKmz extends Omit<Partial<IKmz>, OmitirUpdate> {}
