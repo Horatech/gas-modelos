c4b7ccc4e21e48e034191c6429628bb80ac33788
Fernando Cortese <cortesefernando@gmail.com>
2026-01-14 10:23:08 -0300

    Agregar comentarios descriptivos a las propiedades de la interfaz IDispositivoSml

--- src/interfaces/entidades/configs-dispositivo/dispositivoSml.ts
+++ src/interfaces/entidades/configs-dispositivo/dispositivoSml.ts
@@ -1,6 +1,6 @@
 export interface IDispositivoSml {
   // Comunicacion
-  ip?: string;
+  ip?: string; // IP desde la cual se comunica el dispositivo
   port?: number;
 
   // Info de la sim
@@ -9,5 +9,12 @@ export interface IDispositivoSml {
   telefono?: string;
 
   // Configuracion del SML
-  frecuenciaReporte?: number; // Minutos
+  // valveControl?: boolean;
+  calibrationDeviceNodeReading?: number;
+  reportingCycleInterval?: number; // Segundos
+  timezone?: string; // UTC+2 | UTC-3 | etc.
+  ipReporte?: string; // La IP de a donde va a reportar -> 47.92.222.233 :1822
+  pn?: number; // Pulse number
+  maximunMeterReading?: number; // Valor maximo del medidor
+  reportingRange?: number; // 321: 0321 3--3+21/2 ,Means to report randomly in the range of 3-13:30
 }
