5a472951a9a486d50f103f4b29ec391d45a5f96e
Fernando Cortese <cortesefernando@gmail.com>
2026-03-16 14:28:09 -0300

    agregar propiedad 'versionHardware' a la interfaz IFirmware

--- src/interfaces/entidades/firmware.ts
+++ src/interfaces/entidades/firmware.ts
@@ -1,18 +1,24 @@
 export type TipoDispositivoFirmware = "NSP" | "NUC";
+export type VersionHardware = "v2" | "v3";
 export interface IFirmware {
   _id?: string;
   fechaCreacion?: string;
   version?: string;
+  versionHardware?: VersionHardware;
   dispositivo?: TipoDispositivoFirmware;
   archivo?: string;
 }
 
 // CREATE
 type OmitirCreate = "_id";
-export interface ICreateFirmware
-  extends Omit<Partial<IFirmware>, OmitirCreate> {}
+export interface ICreateFirmware extends Omit<
+  Partial<IFirmware>,
+  OmitirCreate
+> {}
 
 // UPDATE
 type OmitirUpdate = "_id";
-export interface IUpdateFirmware
-  extends Omit<Partial<IFirmware>, OmitirUpdate> {}
+export interface IUpdateFirmware extends Omit<
+  Partial<IFirmware>,
+  OmitirUpdate
+> {}
